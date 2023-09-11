0448914bf92718e779bb1f0bcbc6daf27ecb6f36
rajaskrrose <144654535+rajaskrrose@users.noreply.github.com>
2023-09-11 17:15:59 +0530

    Add files via upload

---
+++ Interview/RL.Data/DataModels/PlanUser.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RL.Data.DataModels.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RL.Data.DataModels
+{
+    
+    public class PlanUser : IChangeTrackable
+    {
+        public int ProcedureId { get; set; }
+        public int PlanId { get; set; }
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime UpdateDate { get; set; }
+
+    }
+}
