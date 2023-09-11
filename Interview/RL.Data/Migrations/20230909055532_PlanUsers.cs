655c917bf6c0383d9763bb39519f1e3960f2f955
rajaskrrose <144654535+rajaskrrose@users.noreply.github.com>
2023-09-11 17:17:11 +0530

    Add files via upload

---
+++ Interview/RL.Data/Migrations/20230909055532_PlanUsers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RL.Data.Migrations
+{
+    public partial class PlanUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PlanUsers",
+                columns: table => new
+                {
+                    ProcedureId = table.Column<int>(type: "INTEGER", nullable: false),
+                    PlanId = table.Column<int>(type: "INTEGER", nullable: false),
+                    UserId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    CreateDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdateDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PlanUsers");
+        }
+    }
+}
